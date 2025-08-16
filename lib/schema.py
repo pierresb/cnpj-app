30db588456b5658ec0d807ed5c4e7131a8feee4a
pierresb <pierre.sbarbosa@gmail.com>
2025-08-16 12:37:18 -0300

    Criar schema.py

---
+++ lib/schema.py
@@ -0,0 +1,74 @@
+EMPRESAS_COLS = {
+    "cnpj_basico": "CNPJ BÁSICO",
+    "razao_social": "RAZÃO SOCIAL / NOME EMPRESARIAL",
+    "natureza_juridica": "NATUREZA JURÍDICA",
+    "qualif_responsavel": "QUALIFICAÇÃO DO RESPONSÁVEL",
+    "capital_social": "CAPITAL SOCIAL DA EMPRESA",
+    "porte": "PORTE DA EMPRESA",
+    "efr": "ENTE FEDERATIVO RESPONSÁVEL",
+}
+
+ESTABELECIMENTOS_COLS = {
+    "cnpj_basico": "CNPJ BÁSICO",
+    "cnpj_ordem": "CNPJ ORDEM",
+    "cnpj_dv": "CNPJ DV",
+    "id_matriz_filial": "IDENTIFICADOR MATRIZ/FILIAL",
+    "nome_fantasia": "NOME FANTASIA",
+    "situacao": "SITUAÇÃO CADASTRAL",
+    "data_situacao": "DATA SITUAÇÃO CADASTRAL",
+    "motivo_situacao": "MOTIVO SITUAÇÃO CADASTRAL",
+    "cidade_exterior": "NOME DA CIDADE NO EXTERIOR",
+    "pais": "PAIS",
+    "data_inicio_atividade": "DATA DE INÍCIO ATIVIDADE",
+    "cnae_principal": "CNAE FISCAL PRINCIPAL",
+    "cnae_secundaria": "CNAE FISCAL SECUNDÁRIA",
+    "tipo_logradouro": "TIPO DE LOGRADOURO",
+    "logradouro": "LOGRADOURO",
+    "numero": "NÚMERO",
+    "complemento": "COMPLEMENTO",
+    "bairro": "BAIRRO",
+    "cep": "CEP",
+    "uf": "UF",
+    "municipio": "MUNICÍPIO",
+    "ddd_1": "DDD 1",
+    "telefone_1": "TELEFONE 1",
+    "ddd_2": "DDD 2",
+    "telefone_2": "TELEFONE 2",
+    "ddd_fax": "DDD DO FAX",
+    "fax": "FAX",
+    "email": "CORREIO ELETRÔNICO",
+    "situacao_especial": "SITUAÇÃO ESPECIAL",
+    "data_situacao_especial": "DATA DA SITUAÇÃO ESPECIAL",
+}
+
+SOCIOS_COLS = {
+    "cnpj_basico": "CNPJ BÁSICO",
+    "ident_socio": "IDENTIFICADOR DE SÓCIO",
+    "nome_razao": "NOME DO SÓCIO (NO CASO PF) OU RAZÃO SOCIAL (NO CASO PJ)",
+    "cpf_cnpj_socio": "CNPJ/CPF DO SÓCIO",
+    "qualif_socio": "QUALIFICAÇÃO DO SÓCIO",
+    "data_entrada_soc": "DATA DE ENTRADA SOCIEDADE",
+    "pais": "PAIS",
+    "cpf_rep_legal": "REPRESENTANTE LEGAL",
+    "nome_rep_legal": "NOME DO REPRESENTANTE",
+    "qualif_rep_legal": "QUALIFICAÇÃO DO REPRESENTANTE LEGAL",
+    "faixa_etaria": "FAIXA ETÁRIA",
+}
+
+SIMPLES_COLS = {
+    "cnpj_basico": "CNPJ BÁSICO",
+    "opcao_simples": "OPÇÃO PELO SIMPLES",
+    "data_opcao_simples": "DATA DE OPÇÃO PELO SIMPLES",
+    "data_exclusao_simples": "DATA DE EXCLUSÃO DO SIMPLES",
+    "opcao_mei": "OPÇÃO PELO MEI",
+    "data_opcao_mei": "DATA DE OPÇÃO PELO MEI",
+    "data_exclusao_mei": "DATA DE EXCLUSÃO DO MEI",
+}
+
+DOMINIOS = {
+    "paises": {"codigo": "CÓDIGO", "descricao": "DESCRIÇÃO"},
+    "municipios": {"codigo": "CÓDIGO", "descricao": "DESCRIÇÃO"},
+    "qualificacoes": {"codigo": "CÓDIGO", "descricao": "DESCRIÇÃO"},
+    "naturezas": {"codigo": "CÓDIGO", "descricao": "DESCRIÇÃO"},
+    "cnaes": {"codigo": "CÓDIGO", "descricao": "DESCRIÇÃO"},
+}
