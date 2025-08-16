29582938d38e91a2129224a9dca5cf9763608f3b
pierresb <pierre.sbarbosa@gmail.com>
2025-08-16 18:46:49 -0300

    Atualizar o streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,11 @@
 #   [server]
 #   maxUploadSize = 500
 
+# no topo do app.py
+from lib.ui import inject_global_css
+# ...
+inject_global_css()
+
 from pathlib import Path
 import io
 import streamlit as st
